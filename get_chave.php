54cbf5ab597bbd4192d2047c5f0b006ed7953dee
B2XMAGA <155447052+B2XMAGA@users.noreply.github.com>
2026-01-04 16:19:55 -0300

    testeconexao

--- get_chave.php
+++ get_chave.php
@@ -1,2 +1,26 @@
 <?php
 echo OPENSSL_VERSION_TEXT . PHP_EOL;
+
+echo 'deploy-001';
+
+
+
+$host = 'a8484c4s4cgoskk0g8scsk08';
+$port = 3306;
+$db   = 'default';
+$user = 'mariadb';
+$pass = '7j8pMMnbypXjvUPx58c9gBFCrY9bSMewrvJSyARBadSQ3pRNktfJjjJuntMmYoix';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ]);
+
+    echo "✅ Conectado com sucesso!";
+} catch (PDOException $e) {
+    echo "❌ Erro na conexão: " . $e->getMessage();
+}
